e2fdc7bdf3f28c022fb2f8a0b9073792ff474411
ТвоёИмяНаGitHub <_email@.com>
2025-09-18 21:38:09 +0300

    Обновление

--- lib/lib/config.dart
+++
@@ -1,5 +0,0 @@
-class Config {
-  static const String supabaseUrl = 'https://tpwjupuaflpswdvudexi.supabase.co';
-  static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwd2p1cHVhZmxwc3dkdnVkZXhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgwMzk2NDAsImV4cCI6MjA3MzYxNTY0MH0.hKSB7GHtUWS1Jyyo5pGiCe2wX2OBvyywbbG7kjo62fo';
-}
